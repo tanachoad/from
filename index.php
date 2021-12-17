52993a62058ba3542b0311da46d49476fe044eb6
tanachoad <80100548+tanachoad@users.noreply.github.com>
2021-12-17 15:03:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 </head>
 
 <body>
-    <h1 style="text-align: center;">แบบฟอร์มกรอกข้อมูลส่วนตัว</h1>
-    <form action="#" method="post" name="add" class="form-horizontal" id="add">
+    <h1 align="center">แบบฟอร์มกรอกข้อมูลส่วนตัว</h1>
+    <form align="center" action="#" method="post" name="add" class="form-horizontal" id="add">
         </p>
         <div class="form-group">
             <div class="col-sm-2 control-label">
